130c387a12d56799efb3ad8ed9313f2939a2632a
hai061898 <truonghaiauto98@gmail.com>
2021-11-20 15:39:11 +0700

    register_controller

---
+++ src/controllers/register_controller.js
@@ -0,0 +1,44 @@
+const { response } = require('express'); 
+const pool = require('../DataBase/DataBase');
+//gọi thư viên database
+const bcrypt = require('bcrypt');
+//gọi thư viên mã hóa 
+
+
+const createUsers = async ( req, res = response ) => {
+
+    const { username, email, passwordd } = req.body;
+    // tạo 1 array request 
+
+    const salt = bcrypt.genSaltSync();
+    // biến khởi tạo hàm băm
+    const pass = bcrypt.hashSync( passwordd, salt );
+    //gọi hàm mã hóa thực thi mã hóa password 
+
+    const hasEmail = await pool.query('SELECT email FROM users WHERE email = ?', [email]);
+    // gọi câu lệnh sql 
+    if( hasEmail.length == 0 ){
+        //nếu mảng = 0 
+        await pool.query(`CALL SP_REGISTER_USER(?,?,?);`, [ username, email, pass ]);
+        // gọi producedure và tham số
+        return res.json({
+            //trả về json 
+            resp: true, // phản hồi true 
+            msg: 'User' + username + 'was created successfully!'
+        });
+    
+    } else {
+        //ngược lại trả về false  
+        return res.json({
+            resp: false,
+            msj : 'The mail is already registered!'
+        }); 
+    }
+};
+
+
+
+module.exports = {
+    createUsers
+};
+//gọi tên file cho việc sử dụng ở các file khác
