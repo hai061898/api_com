55ae17c91ff6653eea46b6618d8d89e28105b12e
hai061898 <truonghaiauto98@gmail.com>
2021-11-21 15:42:48 +0700

    controller

---
+++ src/controllers/home_controller.js
@@ -0,0 +1,95 @@
+const { response } = require('express');
+const pool = require('../dataBase/dataBase');
+
+const HomeCarouseSilder = async (req, res= response) =>{
+    const rows = await pool.query('SELECT * FROM Home_carousel');
+    if( rows.length > 0 ){
+        // >0 có nghĩ giống như tồn tại
+
+        return res.json({
+            resp: true,
+            msj: 'List Images - Home ',
+            slider: rows
+        });
+    
+    } else {
+
+        return res.json({
+            resp: false,
+            msj: 'Without List of images'
+        });
+    }
+}
+
+const ListCategoriesHome =  async ( req, res = response ) => {
+
+    const categories = await pool.query('SELECT * FROM category LIMIT 10');
+
+    if( categories.length > 0 ){
+
+        return res.json({
+            resp: true,
+            msj : 'List categories',
+            categories : categories
+        });
+
+    } else {
+        return res.json({
+            resp: false,
+            msj : 'Without List of Categories'
+        });
+    }
+}
+
+
+const ListProductsHome = async ( req, res = response ) => {
+
+    const products = await pool.query(`CALL SP_LIST_PRODUCTS_HOME();`);
+
+    if( products[0].length > 0 ){
+
+        return res.json({
+            resp: true,
+            msj : 'List Products Home',
+            products: products[0]
+        });
+
+    } else {
+        
+        return res.json({
+            resp: false,
+            msj : 'Without List Products' 
+        });
+    }
+}
+
+
+const ListCategoriesAll = async ( req, res = response ) => {
+
+    const category = await pool.query('SELECT * FROM Category');
+
+    if( category.length > 0 ){
+
+        return res.json({
+            resp: true,
+            msj : 'List Categories All',
+            categories : category
+        });
+
+    } else {
+        return res.json({
+            resp: false,
+            msj : 'Witout List Categories' 
+        }); 
+    }
+}
+
+
+
+
+module.exports = {
+    HomeCarouselSilder,
+    ListCategoriesHome,
+    ListProductsHome,
+    ListCategoriesAll
+}
